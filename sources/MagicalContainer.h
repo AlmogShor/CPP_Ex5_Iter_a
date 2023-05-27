b8d7184759bf5aae784380b4a975b729cf51ef04
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-05-27 18:18:22 +0300

    header and cpp file first edition

--- sources/MagicalContainer.h
+++ sources/MagicalContainer.h
@@ -2,7 +2,70 @@
 // Created by shora on 27/05/2023.
 //
 
-#ifndef CPP_EX5_ITER_A_ITERATOR_H
-#define CPP_EX5_ITER_A_ITERATOR_H
+// MagicalContainer.hpp
+#pragma once
+#include <vector>
+#include <iterator>
+#include <exception>
 
-#endif //CPP_EX5_ITER_A_ITERATOR_H
+class AscendingIterator;
+class SideCrossIterator;
+class PrimeIterator;
+
+class MagicalContainer {
+public:
+    typedef int value_type;
+    typedef AscendingIterator iterator;
+    typedef SideCrossIterator cross_iterator;
+    typedef PrimeIterator prime_iterator;
+
+    MagicalContainer();
+    ~MagicalContainer();
+
+    void add(int element);
+    void remove(int element);
+    int size() const;
+
+    iterator begin();
+    iterator end();
+
+    cross_iterator cross_begin();
+    cross_iterator cross_end();
+
+    prime_iterator prime_begin();
+    prime_iterator prime_end();
+
+private:
+    std::vector<int> elements;
+
+    void sort();
+    bool is_prime(int num);
+
+    friend class AscendingIterator;
+    friend class SideCrossIterator;
+    friend class PrimeIterator;
+};
+
+class AscendingIterator : public std::iterator<std::forward_iterator_tag, int> {
+public:
+    AscendingIterator();
+    AscendingIterator(const AscendingIterator& other);
+    ~AscendingIterator();
+
+    AscendingIterator& operator=(const AscendingIterator& other);
+
+    bool operator==(const AscendingIterator& other) const;
+    bool operator!=(const AscendingIterator& other) const;
+    bool operator>(const AscendingIterator& other) const;
+    bool operator<(const AscendingIterator& other) const;
+
+    int& operator*();
+    AscendingIterator& operator++();
+
+private:
+    std::vector<int>::iterator current;
+
+    friend class MagicalContainer;
+};
+
+// Define similar classes for SideCrossIterator and PrimeIterator
--- sources/MagicalContasiner.cpp
+++ sources/MagicalContasiner.cpp
@@ -3,3 +3,81 @@
 //
 
 #include "MagicalContainer.h"
+// MagicalContainer.cpp
+//#include "MagicalContainer.hpp"
+#include <algorithm>
+
+MagicalContainer::MagicalContainer() {}
+
+MagicalContainer::~MagicalContainer() {}
+
+void MagicalContainer::add(int element) {
+    elements.push_back(element);
+    sort();
+}
+
+void MagicalContainer::remove(int element) {
+    elements.erase(std::remove(elements.begin(), elements.end(), element), elements.end());
+    sort();
+}
+
+int MagicalContainer::size() const {
+    return elements.size();
+}
+
+MagicalContainer::iterator MagicalContainer::begin() {
+    return iterator(elements.begin());
+}
+
+MagicalContainer::iterator MagicalContainer::end() {
+    return iterator(elements.end());
+}
+
+// Similar methods for cross_begin/cross_end and prime_begin/prime_end
+
+void MagicalContainer::sort() {
+    std::sort(elements.begin(), elements.end());
+}
+
+bool MagicalContainer::is_prime(int num) {
+    // implement a method to check if a number is prime
+}
+
+AscendingIterator::AscendingIterator(std::vector<int>::iterator iter) : current(iter) {}
+
+AscendingIterator::AscendingIterator(const AscendingIterator& other) : current(other.current) {}
+
+AscendingIterator::~AscendingIterator() {}
+
+AscendingIterator& AscendingIterator::operator=(const AscendingIterator& other) {
+    current = other.current;
+    return *this;
+}
+
+bool AscendingIterator::operator==(const AscendingIterator& other) const {
+    return current == other.current;
+}
+
+bool AscendingIterator::operator!=(const AscendingIterator& other) const {
+    return !(*this == other);
+}
+
+bool AscendingIterator::operator>(const AscendingIterator& other) const {
+    // implement this
+}
+
+bool AscendingIterator::operator<(const AscendingIterator& other) const {
+    // implement this
+}
+
+int& AscendingIterator::operator*() {
+    return *current;
+}
+
+AscendingIterator& AscendingIterator::operator++() {
+    ++current;
+    return *this;
+}
+
+// Similar implementation for SideCrossIterator and PrimeIterator
+
