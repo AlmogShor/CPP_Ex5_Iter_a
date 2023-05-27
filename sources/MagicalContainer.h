95728b1cb61fc21bf6acb74769fa1dca1ac2c6ec
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-05-27 18:14:43 +0300

    init

---
+++ sources/MagicalContainer.h
@@ -0,0 +1,8 @@
+//
+// Created by shora on 27/05/2023.
+//
+
+#ifndef CPP_EX5_ITER_A_ITERATOR_H
+#define CPP_EX5_ITER_A_ITERATOR_H
+
+#endif //CPP_EX5_ITER_A_ITERATOR_H
---
+++ sources/MagicalContasiner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shora on 27/05/2023.
+//
+
+#include "MagicalContainer.h"
