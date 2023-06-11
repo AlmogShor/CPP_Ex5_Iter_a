4476bde0fc9fc12cb4322d66ace4ec2c08bcc9e0
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-06-12 00:39:25 +0300

    update

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -14,45 +14,101 @@ using namespace ariel;
 /*  MagicalContainer class implementation           */
 /****************************************************/
 
-// ctors
-MagicalContainer::MagicalContainer() : ascendingHead(nullptr), ascendingEnd(nullptr), descendingHead(nullptr),
-                                       descendingEnd(nullptr), primeHead(nullptr), primeEnd(nullptr), _size(0) {
-    this->ascendingEnd = new Node<int>();
-    this->descendingEnd = new Node<int>();
-    this->primeEnd = new Node<int>();
+// MagicalContainers ctors
+MagicalContainer::MagicalContainer()
+        : ascendingHead(nullptr), ascendingEnd(nullptr),
+          descendingHead(nullptr), descendingEnd(nullptr),
+          primeHead(nullptr), primeEnd(nullptr),
+          _size(0) {}
 
+MagicalContainer::MagicalContainer(const MagicalContainer &other)
+        : _size(other._size) {
+    // TODO: Copy the linked lists. This will likely involve creating new nodes
+    // and setting their next pointers to create a copy of the original list.
 }
 
+MagicalContainer::MagicalContainer(MagicalContainer &&other) noexcept
+: ascendingHead(other.ascendingHead),
+ascendingEnd(other.ascendingEnd),
+descendingHead(other.descendingHead),
+descendingEnd(other.descendingEnd),
+primeHead(other.primeHead),
+primeEnd(other.primeEnd),
+_size(other._size) {
+// Reset other's pointers to ensure it doesn't delete the nodes when it's destructed
+other.ascendingHead = other.ascendingEnd = nullptr;
+other.descendingHead = other.descendingEnd = nullptr;
+other.primeHead = other.primeEnd = nullptr;
+other._size = 0;
+}
 
-//private methods
+// The destructor should delete all the nodes to prevent memory leaks
+MagicalContainer::~MagicalContainer() {
+    // TODO: Delete all nodes
+}
+
+// MagicalContainer methods
 bool MagicalContainer::isPrime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    if (num == 2) {
-        return true;
-    }
-    for (int i = 3; i * i <= num; i += 2) {
-        if (num % i == 0) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 == 0 || num % 3 == 0) return false;
+
+    for (int i = 5; i * i <= num; i = i + 6) {
+        if (num % i == 0 || num % (i + 2) == 0)
             return false;
-        }
     }
     return true;
 }
 
 int MagicalContainer::exists(int num) {
-    Node<int> *curr = this->ascendingHead;
+    Node<int> *current = ascendingHead;
     int index = 0;
-    while (curr != nullptr) {
-        if (curr->data == num) {
-            return index;
-        }
-        curr = curr->next;
+    while (current) {
+        if (current->data == num) return index;
+        current = current->next;
         index++;
     }
     return -1;
 }
 
+void MagicalContainer::addElement(int num) {
+    if (exists(num) == -1) {
+        Node<int>::insertAtPosition(new Node<int>(num), 0, &ascendingHead);
+        _size++;
+    }
+}
+
+void MagicalContainer::removeElement(int num) {
+    int index = exists(num);
+    if (index != -1) {
+        Node<int>::removeAtPosition(index, &ascendingHead);
+        _size--;
+    }
+}
+
+int MagicalContainer::size() {
+    return _size;
+}
 
 
+/****************************************************/
+/*  AscendingIterator class implementation           */
+/****************************************************/
 
+// AscendingIterator ctors
+MagicalContainer::AscendingIterator::AscendingIterator()
+        : container(nullptr), currElement(nullptr) {}
+
+MagicalContainer::AscendingIterator::AscendingIterator(const MagicalContainer &container)
+        : container(&container), currElement(container.ascendingHead) {}
+
+MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator &other)
+        : container(other.container), currElement(other.currElement) {}
+
+MagicalContainer::AscendingIterator::AscendingIterator(AscendingIterator &&other) noexcept
+: container(other.container),
+currElement(other.currElement) {
+other.container = nullptr;
+other.currElement = nullptr;
+}
--- sources/myLinkedList.hpp
+++ sources/myLinkedList.hpp
@@ -72,7 +72,63 @@ public:
 
     LinkedList() : head(nullptr) {}
 
-    // More LinkedList functionality goes here
-    // Including implementation of various types of iterators
+    //methods
+    template <typename T>
+    void Node<T>::insertAtPosition(Node<T>* toAdd, int position, Node<T>** head) {
+        // Special case for the head end
+        if (position == 0) {
+            toAdd->next = *head;
+            *head = toAdd;
+            return;
+        }
+
+        // Locate the node before the point of insertion
+        Node<T>* prev = *head;
+        for (int i = 0; prev != nullptr && i < position - 1; i++) {
+            prev = prev->next;
+        }
+
+        // Check if the given position is greater than the number of nodes
+        if (prev == nullptr || prev->next == nullptr) {
+            throw std::invalid_argument("Position out of range");
+        }
+
+        // Insert node after the previous node
+        toAdd->next = prev->next;
+        prev->next = toAdd;
+    }
+
+    template <typename T>
+    void Node<T>::removeAtPosition(int position, Node<T>** head) {
+        Node<T>* temp = *head;
+
+        // If head node itself is to be deleted
+        if (position == 0) {
+            *head = temp->next;
+            delete temp;
+            return;
+        }
+
+        // Find previous node of the node to be deleted
+        for (int i = 0; temp != nullptr && i < position - 1; i++) {
+            temp = temp->next;
+        }
+
+        // If position is more than number of nodes
+        if (temp == nullptr || temp->next == nullptr) {
+            throw std::invalid_argument("Position out of range");
+        }
+
+        // Node temp->next is the node to be deleted, store pointer to the next of node to be deleted
+        Node<T>* next = temp->next->next;
+
+        // Delete node at position (skip from list)
+        delete temp->next;
+
+        // Unlink the deleted node from list
+        temp->next = next;
+    }
+
+
 };
 
