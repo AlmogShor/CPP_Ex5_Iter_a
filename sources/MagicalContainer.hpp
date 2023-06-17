15bec38f3fe67a44c87a5178935713ae74eb1ebf
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-06-17 12:52:26 +0300

    using inline for isPrime function! noice!

--- sources/MagicalContainer.hpp
+++ sources/MagicalContainer.hpp
@@ -11,7 +11,7 @@
 
 #include "linkedList.hpp"
 
-bool isPrime(int n) {
+inline bool isPrime(int n) {
     if (n <= 1) return false;
     if (n <= 3) return true;
     if (n % 2 == 0 || n % 3 == 0) return false;
